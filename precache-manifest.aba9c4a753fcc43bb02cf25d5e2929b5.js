4743035d09fba5785060321da850ec65c4eab737
Effiong Benjamin <benjamineffiong124@yahoo.com>
2019-12-09 21:57:36 +0100

    Updates

---
+++ precache-manifest.aba9c4a753fcc43bb02cf25d5e2929b5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "782f3edf2d24e2b06a0206352c60fae0",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "84e38bfef4b03ff59211",
+    "url": "/monsters-rolodex/static/css/main.ce50dfba.chunk.css"
+  },
+  {
+    "revision": "77115cfbcec72719740d",
+    "url": "/monsters-rolodex/static/js/2.7db90bec.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/monsters-rolodex/static/js/2.7db90bec.chunk.js.LICENSE"
+  },
+  {
+    "revision": "84e38bfef4b03ff59211",
+    "url": "/monsters-rolodex/static/js/main.2af72465.chunk.js"
+  },
+  {
+    "revision": "517fd9319d43d3a57612",
+    "url": "/monsters-rolodex/static/js/runtime-main.ae75c310.js"
+  }
+]);
